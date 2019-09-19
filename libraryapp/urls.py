c5b32a17bc098758a848581c92f0bd295316fd17
Shane Miller <shanemiller89@gmail.com>
2019-09-19 15:32:35 -0500

    added detail view for books, librarians and libraries

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -7,8 +7,11 @@ app_name = "libraryapp"
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^books$', book_list, name='books'),
+    path('books/<int:book_id>/', book_details, name='book'),
     url(r'^librarians$', list_librarians, name='librarians'),
+    path('librarians/<int:librarian_id>/', librarian_details, name='librarian'),
     url(r'^libraries$', list_libraries, name='libraries'),
+    path('libraries/<int:library_id>/', library_details, name='library'),
     url(r'accounts/', include('django.contrib.auth.urls')),
     url(r'^logout/$', logout_user, name='logout'),
     path('book/form', book_form, name='book_form'),
